782bacf71cd78f8c05f57c60085547a132f37088
Joona <joona.kauranen@gmail.com>
2022-11-26 15:13:16 +0200

    testeja

--- tests/test_alkuluku_generoija.py
+++ tests/test_alkuluku_generoija.py
@@ -5,10 +5,10 @@ class test_avain_generoija(unittest.TestCase):
     def setUp(self):
         self.alkulukugeneroija = AlkulukuGeneroija()
 
-    def test_ei_hyvaksy_pienta_yhdistettya_lukua(self):
+    def test_miller_rabin_ei_hyvaksy_pienta_yhdistettya_lukua(self):
         hyvaksyttiinko = self.alkulukugeneroija.apufunktio(12)
         self.assertEqual(hyvaksyttiinko, 0)
 
-    def test_ei_hyvaksy_suurta_yhdistettya_lukua(self):
+    def test_miller_rabin_ei_hyvaksy_suurta_yhdistettya_lukua(self):
         hyvaksyttiinko = self.alkulukugeneroija.apufunktio(1200000000000000000000000000000)
         self.assertEqual(hyvaksyttiinko, 0)
